4083f071cf784bb6272e6e7af29c672a0253189e
endashaw demsis <endashawdemsis@gmail.com>
2023-11-02 03:52:40 +0300

    Minor Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -73,18 +73,7 @@
             <div
               id="navlinks"
               class="invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none"
-            >
-              <div class="mt-12 lg:mt-0">
-                <router-link
-                  to="/login"
-                  class="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
-                >
-                  <span class="relative text-sm font-semibold text-white">
-                    Get Started</span
-                  >
-                </router-link>
-              </div>
-            </div>
+            ></div>
           </div>
         </div>
       </nav>
@@ -468,26 +457,6 @@
               <p class="text-center text-xl text-gray-600 dark:text-gray-300">
                 Be part of interactivity.
               </p>
-              <div class="flex flex-wrap justify-center gap-6">
-                <a
-                  href="https://markly-demo.authui.site/"
-                  class="relative flex h-12 w-full items-center justify-center px-8 before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
-                >
-                  <span
-                    class="relative text-base font-semibold text-white dark:text-dark"
-                    >Get Started</span
-                  >
-                </a>
-                <a
-                  href="#"
-                  class="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
-                >
-                  <span
-                    class="relative text-base font-semibold text-primary dark:text-white"
-                    >More about</span
-                  >
-                </a>
-              </div>
             </div>
           </div>
         </div>
