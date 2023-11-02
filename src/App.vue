430f169e7f3d69ce1a8dbf6ff93135ef62b82e03
endashaw demsis <endashawdemsis@gmail.com>
2023-11-02 05:46:21 +0300

    Auth update

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,20 @@ import { supabase } from "@/webClient/supabase";
 export default {
   name: "App",
   mounted() {
-    supabase.auth.onAuthStateChange((event, session) => {
-      console.log(event, session);
-      console.log("this.$route.name : : ", this.$route.name);
-      if (event === "SIGNED_IN" && this.$route.name === "home") {
-        console.log("SIGNED_IN::::::::::::::::::::::::::::::");
-        this.$router.push({ name: "projects" });
-      } else if (event === "SIGNED_OUT") {
-        this.$router.push({ name: "home" });
-      } else if (event === "INITIAL_SESSION") {
-        if (!session) {
-          this.$router.push({ name: "home" });
-        }
-      }
-    });
+    // supabase.auth.onAuthStateChange((event, session) => {
+    //   console.log(event, session);
+    //   console.log("this.$route.name : : ", this.$route.name);
+    //   if (event === "SIGNED_IN" && this.$route.name === "home") {
+    //     console.log("SIGNED_IN::::::::::::::::::::::::::::::");
+    //     this.$router.push({ name: "projects" });
+    //   } else if (event === "SIGNED_OUT") {
+    //     this.$router.push({ name: "home" });
+    //   } else if (event === "INITIAL_SESSION") {
+    //     if (!session) {
+    //       this.$router.push({ name: "home" });
+    //     }
+    //   }
+    // });
   },
 };
 </script>
